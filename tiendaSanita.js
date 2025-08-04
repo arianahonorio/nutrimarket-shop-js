0d074496533c454eba9410b7b954b807754575d1
Ariana Honorio <hhariana@hotmail.com>
2025-08-03 21:51:20 -0300

    avances-entrega-final

--- tiendaSanita.js
+++ tiendaSanita.js
@@ -9,30 +9,43 @@ const productos = [
     {nombre: "Arroz Integral- 500g" , precio:700, id: 7},
 ]
 
+
 //variables del DOM
 const listaProd = document.getElementById("listaProd")
 const listaCarrito = document.getElementById("listaCarrito")
 const formularioCompra = document.getElementById("formularioCompra")
-const botonCompra = document.getElementById("btnCompra")
 
 //FUNCIONES
 
 //-listado de productos
+
 function mostrarProd(){
-    listaProd.innerHTML = ""
+listaProd.innerHTML = ""
     productos.forEach(prod=>{
-    // Creo elementos
+    //Creo elementos
     const li = document.createElement("li")
     const div = document.createElement("div")
     const btn = document.createElement("button")
     // Modifico
     li.id = prod.id
     div.innerText = `${prod.nombre} - $${prod.precio}`
-    btn.innerText = "Comprar"
+    btn.innerText = "Agregar al carrito"
     btn.addEventListener("click",()=>{
         agregarCarrito(prod)
+        Toastify({
+        text: `Agregado ${prod.nombre} al carrito`,
+        duration: 5000,
+        newWindow: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #6a964c, #efdc97)",
+        }
+        })
+        .showToast();
     })
-    // agregar al DOM
+    //agregar al DOM
     li.appendChild(div)
     li.appendChild(btn)
     listaProd.appendChild(li)
@@ -66,9 +79,9 @@ const guardarCarrito = ()=>{
 //visualizacion de los prroductos agregados al carrito
 const mostrarCarrito = ()=>{
     listaCarrito.innerHTML = ""
-    carrito.forEach(prod=>{
+    carrito.forEach(({nombre, precio})=>{
     const li = document.createElement("li")
-    li.innerHTML = `${prod.nombre} - $${prod.precio}`
+    li.innerHTML = `${nombre} - $${precio}`
     listaCarrito.appendChild(li)
     })
 }
@@ -89,10 +102,19 @@ const agregarCarrito = (prod)=>{
 
 //-finalizacion de compra
 const comprar =(nombre)=>{
-    const agradecimiento = document.getElementById("agradecimiento")
-    agradecimiento.innerText = `Gracias por su compra ${nombre}!`
-    vaciarCarrito()
+    const botonCompra = document.getElementById("btnCompra")
+    botonCompra.addEventListener("click", function(){
+        Swal.fire({
+        title: '¡Compra exitosa!',
+        text: `Gracias por tu compra ${nombre}`,     
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonText: 'Seguir comprando...',
+        cancelButtonText: "Cerrar"
+        })
+    })
 }
+
 
 formularioCompra.addEventListener("submit",(e)=>{
     e.preventDefault()
@@ -101,6 +123,7 @@ formularioCompra.addEventListener("submit",(e)=>{
     console.log(submit)
     comprar(submit.nombre+" "+submit.apellido)
 })
+
 
 //ejecución de funciones
 function inicializar(){
