0cf0825edbde9a09b07ff8e46a2687a4a04cba34
Ariana Honorio <hhariana@hotmail.com>
2025-08-05 21:11:25 -0300

    Entrega final!

--- tiendaSanita.js
+++ tiendaSanita.js
@@ -1,159 +1,109 @@
-//array de productos
-/*const productos = [ 
-    {nombre: "Almendras- 500g", precio:1500, id: 1},
-    {nombre: "Nueces- 1kg" , precio:2700, id: 2},
-    {nombre: "Avena- 200g" , precio:1000, id: 3},
-    {nombre: "Mix semillas- 1kg" , precio:2400, id: 4},
-    {nombre: "Mix frutos secos- 1kg" , precio:3000, id: 5},
-    {nombre: "Harina de almendras- 1kg" , precio:1200, id: 6},
-    {nombre: "Arroz Integral- 500g" , precio:700, id: 7},
-]
-    */
-const apiPropio= "../productos.json"
-const listaProd = document.getElementById("productos")
+//variables
+const apiPropio= "./productos.json";
+const listaProd = document.getElementById("productos");
+const listaCarrito = document.getElementById("listaCarrito");
+const formularioCompra = document.getElementById("formularioCompra");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-function mostrarProductos (arrayProd){
-        listaProd.innerHTML=""
-        arrayProd.forEach(prod =>{
-            const {nombre, precio, id} = prod
-            //Creo elementos
-    const li = document.createElement("li")
-    const div = document.createElement("div")
-    const btn = document.createElement("button")
-    // Modifico
-    li.id = prod.id
-    div.innerText = `${prod.nombre} - $${prod.precio}`
-    btn.innerText = "Agregar al carrito"
-    btn.addEventListener("click",()=>{
-        agregarCarrito(prod)
-        Toastify({
-        text: `Agregado ${prod.nombre} al carrito`,
-        duration: 5000,
-        newWindow: true,
-        gravity: "top",
-        position: "right",
-        stopOnFocus: true,
-        style: {
-        background: "linear-gradient(to right, #6a964c, #efdc97)",
-        }
-        })
-        .showToast();
-    })
-    //agregar al DOM
-    li.appendChild(div)
-    li.appendChild(btn)
-    listaProd.appendChild(li)
-    })
-}
-
+//busco y muestro los productos desde mi JSON
 async function buscarProd() {
     try {
-        const datosJSON=await fetch(apiPropio)
-        const datos=await datosJSON.json()
-        mostrarProductos(datos.recordings)
+        const datosJSON=await fetch(apiPropio);
+        const datos=await datosJSON.json();
+        mostrarProductos(datos);
     } catch (error) {
-        console.warn(error)
+        console.warn(error);
     }
-    return datos
 }
 
-buscarProd()
-
-fetch (apiPropio).then(respuesta=>{
-    return respuesta.json()
-}).then (respuesta=>{
-    mostrarProductos(respuesta.recordings)
-})
-
-//variables del DOM
-//const listaProd = document.getElementById("productos")
-const listaCarrito = document.getElementById("listaCarrito")
-const formularioCompra = document.getElementById("formularioCompra")
-
-//FUNCIONES
-
-//-listado de productos
-
-/*function mostrarProd(){
-listaProd.innerHTML = ""
-    productos.forEach(prod=>{
-    //Creo elementos
-    const li = document.createElement("li")
-    const div = document.createElement("div")
-    const btn = document.createElement("button")
-    // Modifico
-    li.id = prod.id
-    div.innerText = `${prod.nombre} - $${prod.precio}`
-    btn.innerText = "Agregar al carrito"
-    btn.addEventListener("click",()=>{
-        agregarCarrito(prod)
-        Toastify({
-        text: `Agregado ${prod.nombre} al carrito`,
-        duration: 5000,
-        newWindow: true,
-        gravity: "top",
-        position: "right",
-        stopOnFocus: true,
-        style: {
-        background: "linear-gradient(to right, #6a964c, #efdc97)",
-        }
-        })
-        .showToast();
-    })
+//muestro productos en el DOM
+function mostrarProductos (arrayProd){
+        listaProd.innerHTML="";
+        arrayProd.forEach(prod =>{
+            const {nombre, precio, id} = prod;
+            //Creo elementos
+            const li = document.createElement("li");
+            const div = document.createElement("div");
+            const btn = document.createElement("button");
+            // Modifico
+            li.id = id;
+            div.innerText = `
+                    <strong>${nombre}</strong><br>
+                    <span>Precio: $${precio}</span><br>
+                    <span>ID: ${id}</span>`;
+            btn.innerText = "Agregar al carrito";
+            btn.style.backgroundColor="#6a964c";
+            btn.style.color= "white";
+            btn.style.padding= "2px 2px 2px 2px";
+            btn.style.fontSize="15px";
+            btn.style.border="2px solid #6a694c";
+            btn.style.borderRadius="20px 20px";
+            btn.addEventListener("click",()=>{
+                agregarCarrito(prod);
+                Toastify({
+                text: `Agregado ${prod.nombre} al carrito`,
+                duration: 5000,
+                newWindow: true,
+                gravity: "top",
+                position: "right",
+                stopOnFocus: true,
+                style: {
+                    background: "linear-gradient(to right, #6a964c, #efdc97)",
+                    }
+                })
+            .showToast();
+        });
     //agregar al DOM
-    li.appendChild(div)
-    li.appendChild(btn)
-    listaProd.appendChild(li)
-    })
-}*/
+    li.appendChild(div);
+    li.appendChild(btn);
+    listaProd.appendChild(li);
+    });
+};
 
-//-carrito de compra
 
-let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+//-carrito de compra-
 
 //suma de productos agregados al carrito
 const calcularTotalCarrito = ()=>{
-    let total = carrito.reduce((suma,elemento)=>{
-    return suma + elemento.precio
-    },0)
-    return total
-}
+    let total = carrito.reduce((suma,elemento)=> suma + elemento.precio,0);
+    return total;
+};
 
 //visualizacion del total
 const mostrarTotal = ()=>{
-    const divTotal = document.getElementById("total")
-    divTotal.innerText = `Total: $${calcularTotalCarrito()}`
-}
+    const divTotal = document.getElementById("total");
+    divTotal.innerText = `Total: $${calcularTotalCarrito()}`;
+};
 
 //guardo datos de la compra
 const guardarCarrito = ()=>{
-    const carritoJSON = JSON.stringify(carrito)
-    localStorage.setItem("carrito", carritoJSON)
-}
+    const carritoJSON = JSON.stringify(carrito);
+    localStorage.setItem("carrito", carritoJSON);
+};
 
 //visualizacion de los prroductos agregados al carrito
 const mostrarCarrito = ()=>{
-    listaCarrito.innerHTML = ""
+    listaCarrito.innerHTML = "";
     carrito.forEach(({nombre, precio})=>{
-    const li = document.createElement("li")
-    li.innerHTML = `${nombre} - $${precio}`
-    listaCarrito.appendChild(li)
-    })
-}
+    const li = document.createElement("li");
+    li.innerHTML = `${nombre} - $${precio}`;
+    listaCarrito.appendChild(li);
+    });
+};
 
 const vaciarCarrito = ()=>{
-    carrito = []
-    guardarCarrito()
-    mostrarCarrito()
-    mostrarTotal()
-}
+    carrito = [];
+    guardarCarrito();
+    mostrarCarrito();
+    mostrarTotal();
+};
 
 const agregarCarrito = (prod)=>{
-    carrito.push(prod)
-    mostrarCarrito()
-    mostrarTotal()
-    guardarCarrito()
-}
+    carrito.push(prod);
+    mostrarCarrito();
+    mostrarTotal();
+    guardarCarrito();
+};
 
 //-finalizacion de compra
 const comprar =(nombre)=>{
@@ -166,25 +116,23 @@ const comprar =(nombre)=>{
         showCancelButton: true,
         confirmButtonText: 'Seguir comprando...',
         cancelButtonText: "Cerrar"
-        })
-    })
-}
-
+        });
+    });
+};
 
 formularioCompra.addEventListener("submit",(e)=>{
     e.preventDefault()
-    const informacion = new FormData(e.target)
-    const submit = Object.fromEntries(informacion)
-    console.log(submit)
-    comprar(submit.nombre+" "+submit.apellido)
-})
+    const informacion = new FormData(e.target);
+    const submit = Object.fromEntries(informacion);
+    comprar(submit.nombre+" "+submit.apellido);
+});
 
 
 //ejecución de funciones
 function inicializar(){
-    mostrarProd()
-    mostrarCarrito()
-    mostrarTotal()
+    buscarProd();
+    mostrarCarrito();
+    mostrarTotal();
 }
 
-inicializar()
+inicializar();
